7ec6c807a1c1c97b3ed82b8d8f6b11451aa76905
Sylwester Karbowski <163668119+s-karbowski@users.noreply.github.com>
2025-02-25 11:32:05 +0100

    Add files via upload

---
+++ Sylwester/Opgave 3/Opgave 3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Opgave_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float weight = 70f;
+
+            float height = 1.75f;
+
+
+            float bMI = weight / (height * height);
+
+
+            Console.WriteLine($"BMI: {bMI:F2}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
