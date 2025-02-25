debee94de10acb5d26be0075bfad93b4e70692f3
Gavin Denver <gavindenver@gmail.com>
2025-02-25 09:42:30 +0100

    Gav changes

---
+++ Gavin/RepetitionsOpgaver/RepetitionsOpgaver/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RepetitionsOpgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Gav test");
+        }
+    }
+}
