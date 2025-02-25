6ed709e89702ba6c82968f6cf79967b12e903235
Sylwester Karbowski <163668119+s-karbowski@users.noreply.github.com>
2025-02-25 12:33:02 +0100

    Add files via upload

---
+++ Sylwester/Opgave 4/Opgave 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+
+namespace Opgave_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 6;
+
+            int numberB = 4;
+
+            int sum = numberA + numberB;
+
+            int dif = numberA - numberB;
+
+            int product = numberA * numberB;
+
+            double quotient = numberA / (double)numberB;
+
+            int remainder = numberA % numberB;
+
+            Console.WriteLine($"A = {numberA} and B = {numberB}");
+
+            Console.WriteLine($"Sum: {sum}, difference: {dif}, produkt: {product}, kvotient: {quotient:F2} og modulus: {remainder}");
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
